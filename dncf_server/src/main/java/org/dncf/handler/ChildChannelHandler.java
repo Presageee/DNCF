ee5790d540587bec8e80a626ddf193e3ac35ef92
linjuntan <ljt1343@gmail.com>
2017-08-16 00:12:28 +0800

    add worker thread group

--- dncf_server/src/main/java/org/dncf/handler/ChildChannelHandler.java
+++ dncf_server/src/main/java/org/dncf/handler/ChildChannelHandler.java
@@ -1,5 +1,7 @@
 package org.dncf.handler;
 
+import io.netty.util.concurrent.DefaultEventExecutorGroup;
+import io.netty.util.concurrent.EventExecutorGroup;
 import org.dncf.protocol.CommonDecoder;
 import org.dncf.protocol.CommonEncoder;
 import org.dncf.service.AccessService;
@@ -20,9 +22,11 @@ public class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
 
     @Override
     protected void initChannel(SocketChannel socketChannel) throws Exception {
+        static final EventExecutorGroup group = new DefaultEventExecutorGroup(Runtime.getRuntime().availableProcessors() + 1);
+
         socketChannel.pipeline().addLast(new CommonEncoder());
         socketChannel.pipeline().addLast(new CommonDecoder());
 
-        socketChannel.pipeline().addLast(new CommonServiceHandler(accessService));
+        socketChannel.pipeline().addLast(group, new CommonServiceHandler(accessService));
     }
 }
